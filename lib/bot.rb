42545c1f7b01f5595c97dfdb25c96f7fd98fadfb
emmy <emmy66418@gmail.com>
2021-03-31 18:45:37 +0200

    Create an inspiration command for showing us inspiration quotes

--- lib/bot.rb
+++ lib/bot.rb
@@ -29,6 +29,15 @@ class Bot
 
                     when '/map'
                         bot.api.send_location(chat_id: message.chat.id, latitude: -37.807416, longitude: 144.985339)
+
+                    when '/inspiration'
+                        values = Inspiration.new
+                        value = values.select_random
+                        bot.api.send_message(chat_id: message.chat.id, text: "#{value['text']}", date: message.date)
+
+                    
+
+        
                            
             end
         end
