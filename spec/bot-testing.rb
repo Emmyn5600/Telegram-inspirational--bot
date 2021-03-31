5a46c1200cea0eb08253f8f355c8a3240accf355
emmy <emmy66418@gmail.com>
2021-03-31 23:30:54 +0200

    Create random_quote rspec testing

--- spec/bot-testing.rb
+++ spec/bot-testing.rb
@@ -5,6 +5,7 @@ RSpec.describe Inspiration do
     describe '#learn_new_thing' do
         let(:learn) { Inspiration.new }
         let(:learn_json) {learn.learn_new_thing}
+        
        
         it 'should return json response when the request is successful' do
             expect(learn_json.class).to eql(Array)
@@ -13,6 +14,16 @@ RSpec.describe Inspiration do
         it 'should return response when an array is not empty' do
             expect(learn_json.length).to_not eql(0)
         end
-        
     end
+
+    describe  '#random_quote' do
+        let(:pick) { Inspiration.new }
+        let(:pick_string) {pick.random_quote}
+
+         it 'does not pick any quote if it is an empty array' do
+            expect(pick_string.length).not_to eql(0) 
+         end
+         
+    end
+
 end
