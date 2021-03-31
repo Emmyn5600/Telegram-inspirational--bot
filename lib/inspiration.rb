25c57b132af2bd801fc658ee8e1d6aeee549dba8
emmy <emmy66418@gmail.com>
2021-03-31 19:30:26 +0200

    Add class Inspiration with their methods

--- lib/inspiration.rb
+++ lib/inspiration.rb
@@ -1,5 +1,28 @@
-class Cool
- def init 
-    puts 'hello world'
- end
+require 'telegram/bot'
+require 'net/http'
+require 'json'
+require_relative 'bot.rb'
+
+
+class Inspiration
+
+    @new_joke = nil
+
+    def initialize
+        @new_joke = @make_the_request
+    end
+
+    def make_the_request
+        url = 'https://type.fit/api/quotes'
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        response = JSON.parse(response)
+        response
+      end
+    
+      def select_random
+        @new_joke = @new_joke.sample
+        @new_joke
+      end 
+
 end
